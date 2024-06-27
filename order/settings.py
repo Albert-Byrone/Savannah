4ea1d55d69a88fbefb205e813a5fa50ba133df4c
Albert Byrone <albertbyrone1677@gmail.com>
2024-06-27 12:42:08 +0300

    add static files

--- order/settings.py
+++ order/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['*', 'localhost', 'render.com']
 
-ALLOWED_HOSTS= os.environ.get("ALLOWED_HOSTS").split(" ")
+# ALLOWED_HOSTS= os.environ.get("ALLOWED_HOSTS").split(" ")
 # Security configuration settings
 
 #  make cookies HttpOnly, which means they are not accessible via JavaScript,
@@ -179,7 +179,7 @@ REST_FRAMEWORK = {
 # }
 #
 
-database_url = os.environ.get("DATABASE_URL").split(" ")
+database_url = os.environ.get("DATABASE_URL")
 DATABASES = {
     'default': dj_database_url.config(
         default=database_url,
@@ -233,11 +233,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
-# STATIC_URL = "static/"
-# STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
